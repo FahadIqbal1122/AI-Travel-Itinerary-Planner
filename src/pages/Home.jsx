92cab98d910d4fe2c050f82783c8b6cd43ad4240
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2025-06-28 21:25:42 +0300

    Add register code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-import Welcome from "../assets/welcome.svg"
+// import Welcome from "../assets/welcome.svg"
 
 const Home = () => {
   let navigate = useNavigate()
